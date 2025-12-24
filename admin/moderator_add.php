492a52fbe05df9f08ee43e39a6a04c0b043748d2
spiderrrrrr <haoshuantan@gmail.com>
2025-12-24 19:40:01 +0800

    updated moderator add

--- admin/moderator_add.php
+++ admin/moderator_add.php
@@ -61,17 +61,8 @@ if (isset($_POST['add_moderator'])) {
 }
 
 $page_title = "Add Eco-Moderator";
+include 'includes/header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $page_title; ?></title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-</head>
-<body>
-<?php
 
 <style>
     .add-container {
@@ -331,6 +322,4 @@ $page_title = "Add Eco-Moderator";
     </div>
 </div>
 
-</body>
-</html>
-<?php $conn->close(); ?>
+<?php include 'includes/footer.php'; ?>
